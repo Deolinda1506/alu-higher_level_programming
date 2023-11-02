b8ca021b499c67093249d18bf0b7c6860c8391e3
Deolinda1506 <g.bogore@alustudent.com>
2023-11-02 06:01:52 -0700

    fine

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):
-    if i != 101 and i != 113:  # Excludes 'e' and 'q' ASCII values
-        print(chr(i), end='')
+for char in range(97, 123):
+    if chr(char) != 'e' and chr(char) != 'q':
+        print(chr(char), end='')
+
+# Ensure the output doesn't include a newline
