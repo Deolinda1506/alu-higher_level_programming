113a85267b278b2cad0de0cf6b060b1d07373070
Deolinda1506 <g.bogore@alustudent.com>
2023-11-02 06:29:23 -0700

    fine

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):
-    print(chr(i), end='')
 
-# Ensure the output doesn't include a newline
+for i in range(97, 123):  # ASCII values for lowercase alphabet (a:97, z:122)
+    print(chr(i), end='')
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+
 def uppercase(s):
     for char in s:
-        if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - 32), end='')
-        else:
-            print(char, end='')
-    print()
+        diff = 32 if ord(char) >= 97 and ord(char) <= 122 else 0
+        print(chr(ord(char) - diff), end='')
+    print()  # New line
+
+# Test cases
+uppercase("best")
+uppercase("Best School 98 Battery street")
