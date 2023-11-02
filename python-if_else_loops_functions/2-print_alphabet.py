d8dfc8fdb1389ebb01125aa532bb53cc00be14d7
Deolinda1506 <g.bogore@alustudent.com>
2023-11-02 11:04:36 -0700

    fine

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 
-for i in range(97, 123):  # ASCII values for lowercase alphabet (a:97, z:122)
-    print(chr(i), end='')
+for c in range(97, 123):
+    print(f"{chr(c)}", end='')
