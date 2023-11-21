c9cb99bf7aa2af0fbde2021a1e1eabd58033efce
Deolinda1506 <g.bogore@alustudent.com>
2023-11-21 11:41:41 -0800

    fine

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry():
+    """Represent base geometry."""
+
+    def area(self):
+        """Area of a geo"""
+        raise Exception("area() is not implemented")
