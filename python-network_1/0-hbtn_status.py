c7efe35bdd2552b976a2f8ea163fe4c8d2f879f7
Deolinda1506 <g.bogore@alustudent.com>
2023-11-29 16:31:49 -0800

    fine

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using urllib package."""
+'''a Python script that fetches a url'''
+
 
 import urllib.request
-from urllib.error import HTTPError
+
 
 if __name__ == '__main__':
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode("utf-8")))
-    except HTTPError as e:
-        print("Error: {}".format(e))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
