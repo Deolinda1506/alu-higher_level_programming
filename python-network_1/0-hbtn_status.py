100bb7730d3755784fb8f0ea01e375b4c00c715b
Deolinda1506 <g.bogore@alustudent.com>
2023-11-30 15:34:43 -0800

    fine

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''a Python script that fetches a url'''
-
-
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
+"""
 import urllib.request
 
 
