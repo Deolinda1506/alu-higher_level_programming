e5caa95f1bef4f745078e58751dd441b0e4c06a4
Deolinda1506 <g.bogore@alustudent.com>
2024-01-31 09:05:58 -0800

    fine

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const myVar ='Python is cool';
+const myVar ='JavaScript is amazing';
+console.log(cIsfun);
+console.log(Python);
+console.log(JavaScript);
+
